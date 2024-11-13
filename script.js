634e0c6aae0b59bd284c7101e35229c2a578412b
Ikhwan Haqim <ikhwanhaqim24@gmail.com>
2024-11-13 16:14:18 +0800

    Add hover effect to each box

---
+++ script.js
@@ -0,0 +1,18 @@
+const container = document.body.querySelector("#container");
+
+// Add 16 divs
+
+const totalDivs = 16 * 16;
+
+for (let i = 0; i < totalDivs; i++) {
+    const div = document.createElement('div');
+    container.appendChild(div);
+
+    div.addEventListener("mouseover", (box) => {
+        div.style.backgroundColor = "red";
+    })
+
+    div.addEventListener("mouseout", (box) => {
+        div.style.backgroundColor = "transparent";
+    })
+}
